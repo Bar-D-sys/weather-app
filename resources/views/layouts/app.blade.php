d17ea53dc405438237c974ca9cb4a7757daceff4
Muhmmad Arslan <rehmat.ali128805@gmail.com>
2026-07-15 20:26:48 +0500

    Add custom Weather App logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,10 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <a href="/" class="navbar-brand">
-                🌤 Weather App
+            <a href="/" class="navbar-brand d-flex align-items-center">
+                <img src="{{ asset('images/logo.png') }}" alt="Weather App Logo" width="40" height="40" class="me-2">
+
+                <span class="fw-bold">Weather App</span>
             </a>
             <button id="themeToggle" class="btn btn-light">
                 🌙 Dark Mode
